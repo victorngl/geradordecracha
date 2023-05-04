c5446d8796e77c7b5487fa07dd17a4e7b3cf0d88
Joao Victor Costa <victor_ngl2@hotmail.com>
2023-05-04 10:33:29 -0300

    ajustes no export da div do cracha

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,8 @@
 'use client'
 
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import { useState } from 'react'
-
 import * as htmlToImage from 'html-to-image';
-
-import { toPng, toJpeg, toBlob, toPixelData, toSvg } from 'html-to-image';
-
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,7 +10,6 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   const [name, setName] = useState<string>('Insira um nome')
   const [previewPhoto, setPreviewPhoto] = useState<string>('')
-
 
   const handleSubmitPhoto = (e) => {
     if (e?.target?.files?.[0]) {
@@ -46,21 +40,33 @@ export default function Home() {
     <div className='bg-gray-400 w-screen  flex justify-center md:p-10 p-2'>
       <div className='bg-zinc-800 rounded-lg w-full p-2'>
         <div className='text-white text-2xl p-4 font-bold justify-center flex mb-10 border-b-2'>
-            Gerador de Carteirinha
+          Gerador de Carteirinha
         </div>
 
-        {/* INICIO DIV DO CRACHA */}
-        <div id="cracha" className='flex justify-center mb-10'>
-          <div id="fundo" className="static bg-[url('/funcionariomodelo.png')] bg-cover bg-white w-[638px] h-[1016px]">
-            <div id="foto" className="bg-cover relative left-[172px] top-[388px] w-[297px] h-[393px] bg-black">
-              <img src={previewPhoto} className='bg-cover'></img>
+
+        <div id="fundo" className='flex justify-center mb-10'>
+
+
+
+          {/* INICIO DIV DO CRACHA */}
+          <div id="cracha" className="bg-[url('/funcionariomodelo.png')] bg-cover bg-white w-[638px] h-[1016px]">
+
+            <div id="foto" className="flex justify-center bg-cover relative w-[297px] h-[393px] left-[172px] top-[388px] bg-black">
+              <img src={previewPhoto} className='bg-cover max-h-[100%] max-w-[100%]'></img>
             </div>
+
+
             <div className='text-black font-bold flex justify-center relative top-[430px]'>
               <p className='text-[32px]'>{name}</p>
             </div>
           </div>
+          {/* FIM DIV DO CRACHA */}
+
+
+
+
         </div>
-        {/* FIM DIV DO CRACHA */}
+
 
         <div className='text-white border-dashed border-2 p-2 justify-center space-y-4 w-full'>
           <div>
@@ -72,10 +78,10 @@ export default function Home() {
             <label>Insira uma foto: </label>
             <input onChange={(e) => handleSubmitPhoto(e)} name="photo" type='file' />
           </div>
-          
+
           <button className='p-2 rounded text-white bg-blue-700' onClick={e => handleGenerete(e)} type='button'>Gerar Crachá</button>
         </div>
       </div>
-    </div>
+    </div >
   )
 }
