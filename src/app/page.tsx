4b16b05031cd21efacaabf300f3fac3576255f2e
Joao Victor Costa <victor_ngl2@hotmail.com>
2023-05-04 09:16:58 -0300

    Commit inicial gerador de cracha

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,116 +1,81 @@
+'use client'
+
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import { useState } from 'react'
+
+import * as htmlToImage from 'html-to-image';
+
+import { toPng, toJpeg, toBlob, toPixelData, toSvg } from 'html-to-image';
+
 
 const inter = Inter({ subsets: ['latin'] })
 
+
 export default function Home() {
+  const [name, setName] = useState<string>('Insira um nome')
+  const [previewPhoto, setPreviewPhoto] = useState<string>('')
+
+
+  const handleSubmitPhoto = (e) => {
+    if (e?.target?.files?.[0]) {
+
+      const file = e.target.files[0];
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setPreviewPhoto(reader.result as string);
+      }
+      reader.readAsDataURL(file);
+    }
+  }
+
+  const handleGenerete = (e) => {
+
+    var download = require("downloadjs");
+
+    var element = document.getElementById('cracha');
+
+    htmlToImage.toPng(element)
+      .then(function (dataUrl) {
+        download(dataUrl, 'cracha - ' + name + '.png');
+      });
+  }
+
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className='bg-gray-400 w-screen  flex justify-center md:p-10 p-2'>
+      <div className='bg-zinc-800 rounded-lg w-full p-2'>
+        <div className='text-white text-2xl p-4 font-bold justify-center flex mb-10 border-b-2'>
+            Gerador de Carteirinha
+        </div>
+
+        {/* INICIO DIV DO CRACHA */}
+        <div id="cracha" className='flex justify-center mb-10'>
+          <div id="fundo" className="static bg-[url('/funcionariomodelo.png')] bg-cover bg-white w-[638px] h-[1016px]">
+            <div id="foto" className="bg-cover relative left-[172px] top-[388px] w-[297px] h-[393px] bg-black">
+              <img src={previewPhoto} className='bg-cover'></img>
+            </div>
+            <div className='text-black font-bold flex justify-center relative top-[430px]'>
+              <p className='text-[25px]'>{name}</p>
+            </div>
+          </div>
+        </div>
+        {/* FIM DIV DO CRACHA */}
+
+        <div className='text-white border-dashed border-2 p-2 justify-center space-y-4 w-full'>
+          <div>
+            <label>Insira um nome: </label>
+            <input onChange={(e) => setName(e.target.value)} className="text-zinc-800" name="name" type='text' />
+          </div>
+
+          <div>
+            <label>Insira uma foto: </label>
+            <input onChange={(e) => handleSubmitPhoto(e)} name="photo" type='file' />
+          </div>
+          
+          <button className='p-2 rounded text-white bg-blue-700' onClick={e => handleGenerete(e)} type='button'>Gerar Crachá</button>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   )
 }
