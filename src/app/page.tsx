a7ca53a85e6aaf05db60724b13fae44a0b569add
Joao Victor Costa <victor_ngl2@hotmail.com>
2023-05-04 10:56:14 -0300

    ajustes no tamanho da img

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -52,7 +52,7 @@ export default function Home() {
           <div id="cracha" className="bg-[url('/funcionariomodelo.png')] bg-cover bg-white w-[638px] h-[1016px]">
 
             <div id="foto" className="flex justify-center bg-cover relative w-[297px] h-[393px] left-[172px] top-[388px] bg-black">
-              <img src={previewPhoto} className='bg-cover max-h-[100%] max-w-[100%]'></img>
+              <img src={previewPhoto} className='bg-cover w-full max-h-[100%] max-w-[100%]'></img>
             </div>
 
 
